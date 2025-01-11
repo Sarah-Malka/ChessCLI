8e02915721c485b279bb64233b3045395a93fdbf
Erez Ami Feiglin <erezamif@gmail.com>
2025-01-11 23:29:38 +0200

    start move parsing and move authenticating logic

--- Chess/board.h
+++ Chess/board.h
@@ -4,13 +4,18 @@
 
 using array2D = std::vector<std::vector<Square>>;
 
+
 class Board
 {
 private:
 	array2D board;
+	singleMove lastMove;
+	bool WhiteToPlay = true;
 
 	static bool alreadyCreated;
-	static array2D GetInitialState();
+	static array2D GetToInitialState();
+	bool isValidMove(struct singleMove);
+	singleMove StringToMove(char* move);
 
 public:
 	Board();
