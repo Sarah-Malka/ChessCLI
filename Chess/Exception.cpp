aa2d91ee9e8673bc30a9dad7be62181730d646c4
Sarah Malka <sarahmal@g.jct.ac.il>
2025-01-29 22:14:16 +0200

    messages

--- Chess/Exception.cpp
+++ Chess/Exception.cpp
@@ -1,10 +1,16 @@
 #include "Exception.h"
 
-Exception::Exception(const ErrorCode errorCode) : error(errorCode)
+Exception::Exception(const ErrorCode errorCode, const std::wstring errorMessage = L"")
+	: error(errorCode), message(errorMessage)
 {
 }
 
 ErrorCode Exception::GetError() const
 {
 	return error;
+}
+
+std::wstring Exception::GetMessage() const
+{
+	return message;
 }
--- Chess/Exception.h
+++ Chess/Exception.h
@@ -1,12 +1,15 @@
 #pragma once
 #include "error.h"
+#include "string"
 
 class Exception
 {
 private:
 	ErrorCode error;
+	std::wstring message;
 public:
-	Exception(const ErrorCode errorCode);
+	Exception(const ErrorCode errorCode, const std::wstring errorMessage);
 	ErrorCode GetError()const;
+	std::wstring GetMessage()const;
 };
 
