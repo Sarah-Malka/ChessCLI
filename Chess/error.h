a15d48bfe66bc4c24f9402823ac80bf0dca1e110
Sarah Malka <sarahmal@g.jct.ac.il>
2025-03-16 19:03:05 +0200

    error codes and enum map

--- Chess/error.h
+++ Chess/error.h
@@ -1,16 +1,68 @@
 #pragma once
 
+#include <map>
+#include <string>
 
-
-enum class ErrorCode
+enum class ErrorCode //Order Is important
 {
-	PositaionOutOfBoard = 0,
-	NoPieceCanMove,
-	MoreThanOneCompatiblePiece,
-	IllegalChessExposure,
-	CannotTakeHisOwnPiece,
+	PositionOutOfBoard = 0,
+	YouAreInCheck,
 	TargetPositionIsCurrentPosition,
-	CannotCastleWithChess,
+
+	SquaresNotEmptyBetweenKingAndRock,
 	KingAlreadyMoved,
 	RockAlreadyMoved,
+	IllegalCheckExposureDuringCastlation,
+	KingCanMoveOnlyOneSquare,
+
+	
+	CannotGoTrhoughOtherPieces,
+	RockMustStayInRowOrColumn,
+	BishopMustStayInHisDiagonal,
+
+	PawnAlreadyMoved,
+	PawnMustMoveForward,
+	SquareIsOccupied,
+	NoOpponentPieceToEat,
+	PawnCanMoveOnlyOneSquare,
+
+	InvalidQueenMove,
+	InvalidKnightMove,
+	InvalidPawnMove,
+	
+	SquareOccupiedByYourOwnPiece,
+
+	IllegalCheckExposure,
+	NoPieceFoundAtOriginPosition,
+	MoreThanOneCompatiblePiece,
+
+	Success
 };
+
+static std::map<ErrorCode, std::wstring> errorCodeToMessage = {
+	{ErrorCode::Success, L"Success"},
+	{ErrorCode::PositionOutOfBoard, L"PositaionOutOfBoard"},
+	{ErrorCode::TargetPositionIsCurrentPosition, L"TargetPositionIsCurrentPosition"},
+	{ErrorCode::SquaresNotEmptyBetweenKingAndRock, L"SquaresNotEmptyBetweenKingAndRock"},
+	{ErrorCode::KingAlreadyMoved, L"KingAlreadyMoved"},
+	{ErrorCode::RockAlreadyMoved, L"RockAlreadyMoved"},
+	{ErrorCode::IllegalCheckExposureDuringCastlation, L"IllegalCheckExposureDuringCastlation"},
+	{ErrorCode::KingCanMoveOnlyOneSquare, L"KingCanMoveOnlyOneSquare"},
+	{ErrorCode::RockMustStayInRowOrColumn, L"RockMustStayInRowOrColumn"},
+	{ErrorCode::BishopMustStayInHisDiagonal, L"BishopMustStayInHisDiagonal"},
+	{ErrorCode::CannotGoTrhoughOtherPieces, L"CannotGoTrhoughOtherPieces"},
+	{ErrorCode::PawnAlreadyMoved, L"PawnAlreadyMoved"},
+	{ErrorCode::PawnMustMoveForward, L"PawnMustMoveForward"},
+	{ErrorCode::PawnCanMoveOnlyOneSquare, L"PawnCanMoveOnlyOneSquare"},
+	{ErrorCode::SquareIsOccupied, L"SquareIsOccupied"},
+	{ErrorCode::NoOpponentPieceToEat, L"NoOpponentPieceToEat"},
+	{ErrorCode::InvalidQueenMove, L"InvalidQueenMove"},
+	{ErrorCode::InvalidKnightMove, L"InvalidKnightMove"},
+	{ErrorCode::InvalidPawnMove, L"InvalidPawnMove"},
+	{ErrorCode::SquareOccupiedByYourOwnPiece, L"SquareOccupiedByYourOwnPiece"},
+	{ErrorCode::IllegalCheckExposure, L"IllegalCheckExposure"},
+	{ErrorCode::MoreThanOneCompatiblePiece, L"MoreThanOneCompatiblePiece"},
+	{ErrorCode::YouAreInCheck, L"YouAreInCheck"},
+	{ErrorCode::NoPieceFoundAtOriginPosition, L"NoPieceFoundAtOriginPosition"},
+};
+
